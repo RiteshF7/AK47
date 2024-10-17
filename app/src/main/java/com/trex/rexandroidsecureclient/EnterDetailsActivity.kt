00675e9491c9e695305a1fae4a461e094ac8bec3
ritesh <riteshf7@gmail.com>
2024-10-17 19:00:28 +0530

    still working

--- app/src/main/java/com/trex/rexandroidsecureclient/EnterDetailsActivity.kt
+++ app/src/main/java/com/trex/rexandroidsecureclient/EnterDetailsActivity.kt
@@ -2,11 +2,13 @@ package com.trex.rexandroidsecureclient
 
 import android.app.Activity
 import android.content.Context
+import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
+import com.trex.rexandroidsecureclient.utils.ClientSharedPrefs
 
 class EnterDetailsActivity : Activity() {
     private lateinit var enterImeiEditText: EditText
@@ -16,6 +18,7 @@ class EnterDetailsActivity : Activity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_enter_details)
 
+        startActivity(Intent(this,FinalizeActivity::class.java))
         // Initialize views
         enterImeiEditText = findViewById(R.id.enter_imei)
         createNewDeviceButton = findViewById(R.id.create_new_device)
@@ -32,6 +35,8 @@ class EnterDetailsActivity : Activity() {
 
     private fun createNewDevice(imei: String) {
         getErrorLogs()
+        val log = ClientSharedPrefs().getImei()
+        Log.i("lifecycle:: ", "createNewDevice: ${log}")
 //        NewDeviceCreator().saveAfterGettingImei {
 //            val intent = Intent(this, FinalizeActivity::class.java)
 //            startActivity(intent)
